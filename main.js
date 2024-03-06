bc55c3b9911be37ed0d1302c4128af3e963e83f9
maryam siddique <maryamsiddique860@gmail.com>
2024-03-06 20:12:47 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//cases
+//camelCase
+var userName = "maryam";
+console.log(userName);
+//snake_case
+var user_name = "maryam";
+console.log(user_name);
+//PascalCase
+var UserName = "maryam";
+console.log(UserName);
+// array []
+var students = ["haya", "daniyal", "khadija", "ayzal",];
+console.log(students);
+var members = ["haya", "hamza", "aroob", "hadi",];
+console.log(members);
+var memberslist = [1, 4, 56, 11, 23, 76, 89, 90,];
+console.log(memberslist);
+var areTheyStudents = [true, false,];
+console.log(areTheyStudents);
+// index 0.....
+var baskitlist = ["apple", "peach", "grapes", 3, 7, 9, 10, 11, true, false,];
+console.log(baskitlist, [6], baskitlist, [0]);
+var fruits = ["apple", "banana", "peach", "mango",];
+console.log(fruits, [2]);
+var fruitsNumber = [3, 6, 8, 9, 10, 76,];
+console.log(fruitsNumber, [0], fruitsNumber, [5]);
+var isFruitsBaskit = [true, false, true, false];
+console.log(isFruitsBaskit, [3]);
