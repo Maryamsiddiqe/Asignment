3933895a6b51565c94d5af35dbe87abaf2db446e
maryam siddique <maryamsiddique860@gmail.com>
2024-03-06 19:43:24 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var shopName = "stonegas";
+console.log(shopName);
+var oven_container = 3;
+console.log(oven_container);
+var isGasAppliances = true;
+console.log(isGasAppliances);
+var userName = "haya";
+console.log(userName);
+var age = 4;
+console.log(age);
+var isStudent = false;
+console.log(isStudent);
---
+++ main.ts
@@ -0,0 +1,16 @@
+let shopName: string = "stonegas" ;
+console.log(shopName);
+let oven_container: number = 3;
+console.log(oven_container);
+let isGasAppliances: boolean = true; 
+console.log(isGasAppliances);
+let userName: string = "haya";
+console.log(userName);
+let age: number = 4;
+console.log(age);
+let isStudent: boolean = false;
+console.log(isStudent);
+
+
+
+
