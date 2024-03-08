b3423f05c5d5fa521f1fbfb2de4cdb1d0002ab92
maryam siddique <maryamsiddique860@gmail.com>
2024-03-09 02:41:36 +0500

    secound commit

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
 
+ //camelCase
+ let userName = "maryam";
+ console.log(userName);
+//snake_case
+let user_name = "maryam";
+console.log(user_name);
+//PascalCase
+let UserName = "maryam";
+console.log(UserName);
+// array []
+
+let students = ["haya", "daniyal", "khadija", "ayzal", ];
+console.log(students);
+let members: string[] = ["haya", "hamza", "aroob", "hadi",];
+console.log(members);
+let memberslist: number[] = [1, 4, 56, 11, 23, 76, 89, 90,];
+console.log(memberslist);
+let areTheyStudents: boolean[] = [true , false, ];
+console.log(areTheyStudents);
+
+// index 0.....
+let baskitlist: any[] = ["apple", "peach", "grapes",3,7,9,10,11, true, false, ];
+console.log(baskitlist,[6], baskitlist,[0]);
+let fruits: string[] = ["apple","banana", "peach", "mango", ];
+console.log(fruits, [2]);
+let fruitsNumber: number[] = [3, 6, 8, 9, 10, 76, ];
+console.log(fruitsNumber, [0] , fruitsNumber,[5]);
+let isFruitsBaskit: boolean[] = [true,false, true, false ];
+console.log(isFruitsBaskit , [3]);
+
